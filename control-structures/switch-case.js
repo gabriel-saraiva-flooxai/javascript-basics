0f387429e0767812585bd107bf9194d4d3213fd0
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-20 02:26:51 -0300

    feat: add switch-case examples

---
+++ control-structures/switch-case.js
@@ -0,0 +1,19 @@
+let cor = "amarelo";
+
+switch (cor) {
+    case "vermelho":
+        console.log('Calma calabreso!');
+        break;
+        
+    case "verde":
+        console.log('VAI!');
+        break;
+
+        case "amarelo":
+            console.log('ACELERA!');
+            break;
+
+    default:
+        console.log('Sei lá painho...')
+        break;
+}
