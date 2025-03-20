7aa900f09825a5412a7fe38fe188e321822eb993
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-20 02:28:15 -0300

    feat: add for-loop examples

---
+++ control-structures/for-loop.js
@@ -0,0 +1,12 @@
+// template string ${ js }`
+
+let n = 7;
+
+for (let i = 0; i <= 10; x++) {
+
+    console.log(`${i} x ${n} = ${i*n}`);
+
+    //console.log(i + " x " + n + " = " + (i*n));
+
+}
+  
