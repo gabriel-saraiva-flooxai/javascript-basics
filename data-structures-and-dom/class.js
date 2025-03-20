80b81fdaa0b39b50252815e8ea79f925d96443c8
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-20 02:36:47 -0300

    feat: add class examples

---
+++ data-structures-and-dom/class.js
@@ -0,0 +1,16 @@
+
+class camelo {
+
+    constructor() {
+        this.cor = "tunado";
+    }
+
+    cuspir(){
+        console.log("cê ta todo moiado pai");
+        return "kk";
+    }
+
+}
+
+let objeto = new camelo();
+console.log(objeto.cuspir());
