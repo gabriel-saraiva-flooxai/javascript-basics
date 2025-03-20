05871028580650e552a3fd8fd4477cb7ee848a63
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-03-20 02:25:56 -0300

    feat: add if-else examples

---
+++ control-structures/if-else.js
@@ -0,0 +1,28 @@
+/* var msg = "Hello, world!"
+let (declarar variavel em um escopo)
+
+document.write(msg);
+console.log(msg)
+console.log(msg)
+console.log(msg) 
+*/
+
+/* let a = 10;
+const b = "10";
+
+console.log(a == b); */
+
+let cor = "";
+
+if (cor === "verde") {
+    
+    console.log('siga');
+
+} else if (cor === "amarelo") {
+
+    console.log('atenção');
+    
+} else if ("vermelho") {
+    console.log('pare');
+
+}
